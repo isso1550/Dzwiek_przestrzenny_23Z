c3e80126ccb757b111d1ceaf2f36fb0b5106d500
filiphorst <jafiliph@gmail.com>
2023-11-02 15:59:50 +0100

    v1

--- Aplikacja/panning.py
+++ Aplikacja/panning.py
@@ -1,7 +1,5 @@
-from numpy import pi, sin, sqrt, deg2rad, cos, int16
-from numpy import arange, concatenate, flip, ceil , tile, negative, rad2deg, zeros, add, int16, multiply
-import delay
-from utils import muffle
+from numpy import pi, sin, cos, int16
+from numpy import arange, concatenate, flip, ceil , tile, negative, int16
 
 def Pan(leftSignal, rightSignal, amp_L, amp_R, returnAmplitudes):
     if(returnAmplitudes):
@@ -37,12 +35,7 @@ def ConstantPowerPan(angle, leftSignal, rightSignal, returnAmplitudes = False, p
             amp_L = 0.92
     return Pan(leftSignal, rightSignal, amp_L, amp_R, returnAmplitudes)
 
-def MixedPan(angle, leftSignal, rightSignal, maxAngle = pi, returnAmplitudes = False):
-    #print("-4.5db Panning (mixed)")
-    p = angle/maxAngle
-    amp_L = sqrt( (1-p) * sin( deg2rad((1-p) * pi/2) ) ) 
-    amp_R = sqrt( p * sin( deg2rad( p * deg2rad(pi/2) ) ) )
-    return Pan(leftSignal, rightSignal, amp_L, amp_R, returnAmplitudes)
+
 
 def Pan8d(leftSignal, rightSignal, framerate, step, rot_seconds, method=ConstantPowerPan, progiMin=False, progiMax=False):
     #Utworzenie plynnego przejscia od 0 do 180 stopni z krokiem step
@@ -73,51 +66,17 @@ def Pan8d(leftSignal, rightSignal, framerate, step, rot_seconds, method=Constant
     #Przesun okno -> przeksztalc wycinek dla odpowiadajacego kata -> dolacz do lacznego sygnalu
     if (method == ConstantPowerPan):
         for i in range(0, len(angles)):
-            angle = abs(angles[i])
-            
+            angle = abs(angles[i])     
             new_L, new_R = method(angle, left[i*window:(i+1)*window], right[i*window:(i+1)*window], progiMin=progiMin, progiMax=progiMax)
-            #if delaySig:
-                #new_L, new_R = delay.delay(angle, new_L, new_R, framerate)
-                
-                #try:
-                    #left2[i*window:(i*window)+len(new_L)] = add(left2[i*window:(i*window)+len(new_L)], new_L)
-                    #right2[i*window:(i*window)+len(new_R)] = add(right2[i*window:(i*window)+len(new_R)], new_R)
-                #    left2[i*window:(i+1)*window] = new_L[0:window]
-                #    right2[i*window:(i+1)*window] =  new_R[0:window]
-                #except ValueError as e:
-                    #pass
-                    #left2 = concatenate((left2, new_L))
-                    #print(type(left[0]))
-                    #right2 = concatenate((right2, new_R))
-                    #print(e)
-            #else: 
             left2[i*window:(i+1)*window], right2[i*window:(i+1)*window] = new_L, new_R
-            if angles[i] < 0:
-                
-                #Mozna probowac muffle jak jest za sluchaczem, czy zadziala?
-                #Wn = 22000 - 8000*2*(1-abs(rad2deg(pi/2) - abs(rad2deg(angles[i])))/90)
-                
-                #print(Wn)
-                #new_L = muffle(new_L, framerate, Wn)
-                #new_R = muffle(new_R, framerate, Wn)
-                new_L = new_L
-            #try:
-            #    left[i*window:(i+1)*window], right[i*window:(i+1)*window] = new_L, new_R
-            #except ValueError as e:
-            
+
         left = left2
         right = right2     
     else:
         for i in range(0, len(angles)):
-            angle = abs(angles[i])
-            
+            angle = abs(angles[i])      
             new_L, new_R = method(angle, left[i*window:(i+1)*window], right[i*window:(i+1)*window])
             left[i*window:(i+1)*window], right[i*window:(i+1)*window] = new_L, new_R
-    #if len(left) < len(right):
-    #    left = concatenate((left, zeros(len(right)-len(left), dtype=int16)))
-    #elif len(left) > len(right):
-    #    right = concatenate((right, zeros(len(left)-len(right), dtype=int16)))
-
     return left, right
 
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Projekt_audio
+# Projekt 
+Temat: Dźwięk przestrzenny - teoria i aplikacja
+
+Przedmiot Techniki Multimedialne 2023Z
+
+Struktura:
+* Aplikacja: zawiera pliki zrodlowe aplikacji   
+    * Tworzona na python 3.11.6
+    * DzwiekPrzestrzenny.py to glowny plik wykonywalny
+    * podfolder pomocnicze zawiera notebooki jupyter, ktore sluzyly do generowania odsluchow i wykresow (raczej nie maja znaczenia przy sprawdzaniu)
+    * podfolder output zawiera wersje .exe programu (Jesli jej nie ma, to znaczy ze zostala celowo usuniete ze wzgledu na rozmiar >300mb)
+    * requirements.txt zawiera wymagane bilbioteki pomijając te domyslnie zawarte z interpreterem
+* Dokumenty: zawiera czesc teoretyczna
+    * Sprawozdanie i konspekt w wersjach pdf i docx
+    * podfolder Materialy/Audio zawiera odsluchy do sprawozdanai
+    * podfolder Materialy/Wykresy zawiera wszystkie rysunki ze sprawozdania
+* Audio: zawiera przykladowe pliki dzwiekowe do wlasnych badan
+* HRIR: zawartosc biblioteki 
+
+Link do repozytorium: https://github.com/isso1550/Dzwiek_przestrzenny_23Z
